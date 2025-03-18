7b6fbdc2731149d84a14aeda14a264d0d468656d
Norbert Gabos <dont.vip@gmail.com>
2025-03-18 15:45:30 +0100

    implement task 1 & task 2

--- src/main/scala/u02/Modules.scala
+++ src/main/scala/u02/Modules.scala
@@ -1,27 +1,55 @@
 package u02
 
 object Modules extends App:
-
-  // An ADT: type + module
-  enum Person:
-    case Student(name: String, year: Int)
-    case Teacher(name: String, course: String)
-
-  object Person:
-    def name(p: Person): String = p match
-      case Student(n, _) => n
-      case Teacher(n, _) => n
-
-  println(Person.name(Person.Student("mario", 2015)))
-
-  import Person.*
-
-  println(name(Student("mario", 2015)))
-
-  // a method outside the Person module
-  def isStudent(p: Person): Boolean = p match
-    case Student(_, _) => true
-    case _ => false
-
-  println(isStudent(Student("mario", 2015)))
+    
+    import u03.Sequences.*
+    import u03.Sequences.Sequence.*
+    
+    // An ADT: type + module
+    enum Person:
+        case Student(name: String, year: Int)
+        case Teacher(name: String, course: String)
+    
+    object Person:
+        def name(p: Person): String = p match
+            case Student(n, _) => n
+            case Teacher(n, _) => n
+            
+        def getCourseTeacher(sp: Sequence[Person]): Sequence[String] =
+            val f = (p: Person) => p match
+                case Teacher(_, _) => true
+                case _ => false
+            
+            val m = (p: Person) => p match
+                case Teacher(_, c) => c
+                case _ => ""
+            
+            map(filter(sp)(f))(m)
+        
+        def foldLeft[A](s: Sequence[A])(n: Int = 0)(op: (Int, A) => Int): Int = s match
+            case Cons(h, t) => foldLeft(t)(op(n, h))(op)
+            case _ => n
+        
+        def totalCoursesTaught(s: Sequence[Person]): Int =
+            val f = (p: Person) => p match
+                case Teacher(_, _) => true
+                case _ => false
+            
+            val t = filter(s)(f)
+            val courses = getCourseTeacher(t)
+            
+            foldLeft(courses)(0)((acc, _) => acc + 1)
+    
+    println(Person.name(Person.Student("mario", 2015)))
+    
+    import Person.*
+    
+    println(name(Student("mario", 2015)))
+    
+    // a method outside the Person module
+    def isStudent(p: Person): Boolean = p match
+        case Student(_, _) => true
+        case _ => false
+    
+    println(isStudent(Student("mario", 2015)))
 end Modules
---
+++ src/test/scala/u02/ModulesTest.scala
@@ -0,0 +1,39 @@
+package u02
+
+import org.junit.*
+import org.junit.Assert.*
+
+class ModulesTest:
+
+    import Modules.*
+    import Modules.Person.*
+    import u03.Sequences.*
+    import u03.Sequences.Sequence.*
+    
+    val Viroli = Teacher("Viroli", "PPS")
+    val Ricci =  Teacher("Ricci", "PCD")
+    val Moreno =  Teacher("Moreno", "HPC")
+    val Viroli2 = Teacher("Viroli", "OOP")
+    val Stud1 = Student("Marco", 15)
+    val Stud2 = Student("Angelo", 63)
+    val Stud3 = Student("Matteo", 25)
+    
+    val everyone = Cons(Viroli, Cons(Stud1, Cons(Ricci, Cons(Stud2, Cons(Moreno, Cons(Stud3, Cons(Viroli2, Nil())))))))
+    val teachers = Cons(Viroli, Cons(Ricci, Cons(Moreno, Cons(Viroli2, Nil()))))
+    val students = Cons(Stud1, Cons(Stud2, Cons(Stud3, Nil())))
+    val onlyViroliAllOverAgain = Cons(Viroli, Cons(Viroli, Cons(Viroli, Cons(Viroli, Nil()))))
+    
+    @Test def testCourseTeacher() =
+        assertEquals(Cons("PPS", Cons("PCD", Cons("HPC", Cons("OOP", Nil())))), getCourseTeacher(everyone))
+        assertEquals(Cons("PPS", Cons("PCD", Cons("HPC", Cons("OOP", Nil())))), getCourseTeacher(teachers))
+        assertEquals(Nil(), getCourseTeacher(students))
+        assertEquals(Nil(), Nil())
+        
+    @Test def testFoldLeft() =
+        val list = Cons(3, Cons(7, Cons(1, Cons(5, Nil()))))
+        assertEquals(16, foldLeft(list)(0)(_ + _))
+        assertEquals(-16, foldLeft(list)(0)(_ - _))
+        
+    @Test def testTotalCoursesTaught() =
+        assertEquals(4, totalCoursesTaught(everyone))
+        assertEquals(1, totalCoursesTaught(onlyViroliAllOverAgain))
